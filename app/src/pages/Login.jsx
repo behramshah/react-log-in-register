d89810a75cd27a75d1d958e5aa9948dce5f0149f
Bahram Jabbarov <bahram.pj@code.local>
2022-09-15 12:34:18 +0400

    final

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -8,7 +8,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { signInWithGooglePopup} from '../utils/firebase'
 
 function Copyright(props) {
   return (
