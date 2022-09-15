aeaa6b056527ab1f859b5b92efaaea6043efd9c8
Bahram Jabbarov <bahram.pj@code.local>
2022-09-15 10:34:09 +0400

    routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import LogIn from './pages/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route index path='auth' element={<LogIn/>}/>
+      <Route index path='register' element={<Register/>}/>    
+    </Routes>
   );
 }
 
