61fc83d7a36611ea4c89160c1132a7db0d0a2e06
Bahram Jabbarov <bahram.pj@code.local>
2022-09-15 11:19:24 +0400

    styles float

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
+import RegistrationForm from './pages/Register';
+import SignInSide from './pages/Login';
 import { Routes, Route } from 'react-router-dom';
-import ButtonAppBar from "./pages/Home";
-import SignInSide from "./pages/Login";
-import "./App.css";
+import ButtonAppBar from './pages/Home'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<ButtonAppBar/>}/>
       <Route index path='auth' element={<SignInSide/>}/>    
+      <Route index path='register' element={<RegistrationForm/>}/>    
     </Routes>
 )}
+
 
 export default App;
---
+++ app/src/pages/HomepAge.jsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+
+    return(
+        <h1>Give me Pizza!</h1>
+    )
+}
+
+export default HomePage;
